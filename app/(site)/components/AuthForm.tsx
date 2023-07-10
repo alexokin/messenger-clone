3f9683444c68bf553d89ec82ab72dee51b9f53e4
alexokin <alex.okin@gmail.com>
2023-07-10 18:12:38 +0300

    login functionality and social login done

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,10 @@ import { useCallback, useState } from "react"
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form"
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import AuthSocialButton from "./AuthSocialButton"
+import axios from "axios"
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils"
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -42,16 +46,37 @@ const AuthForm = () => {
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true)
         if (variant === 'REGISTER') {
-            //axios register
+            axios.post('/api/register', data)
+                .catch(() => toast.error('Something went wrong.'))
+                .finally(() => setIsLoading(false))
         }
         if (variant === 'LOGIN') {
             //nextauth signin
+            signIn('credentials', { ...data, redirect: false }).then((callback) => {
+                if (callback?.error) {
+                    toast.error('Invalid credentials')
+                }
+                if (callback?.ok && !callback?.error) {
+                    toast.success('Logged in!')
+                }
+            }).finally(() => {
+                setIsLoading(false)
+            })
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true)
-        //NextAuth social sign in
+        signIn(action,{ redirect:false}).then((callback) => {
+            if (callback?.error) {
+                toast.error('Invalid credentials')
+            }
+            if (callback?.ok && !callback?.error) {
+                toast.success('Logged in!')
+            }
+        }).finally(() => {
+            setIsLoading(false)
+        })
     }
 
     return (
